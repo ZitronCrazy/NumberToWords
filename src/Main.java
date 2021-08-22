aac02662df2a1eab643c035b2baecfae86cb328a
ZitronCrazy <zitro5@protonmail.com>
2021-08-22 21:05:02 +0200

    NumberToWords

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        NumberToWords.numberToWords(-1);
+        NumberToWords.numberToWords(123);
+        NumberToWords.numberToWords(1010);
+        NumberToWords.numberToWords(1000);
+        NumberToWords.numberToWords(-12);
+        NumberToWords.getDigitCount(0);
+        NumberToWords.getDigitCount(123);
+        NumberToWords.getDigitCount(-12);
+        NumberToWords.getDigitCount(5200);
+        NumberToWords.reverse(-121);
+        NumberToWords.reverse(2121);
+        NumberToWords.reverse(1234);
+        NumberToWords.reverse(100);
     }
 }
